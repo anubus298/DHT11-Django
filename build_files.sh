bc67cbb8b5f162f2c2db77fd016b5287da338f05
kira <kira@debian-kira.kira.com>
2024-12-27 13:42:30 +0100

    fixed sh file

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-# Install dependencies
+# Install some dependencies
+apk add --update --no-cache \
+    alpine-sdk \
+    postgresql-dev \
+    libffi-dev
+
+# Install Python dependencies
 python3 -m pip install -r requirements.txt
 
 # Collect static files
 python3 manage.py collectstatic --noinput
 
-# Create Vercel-compatible output vercel directory
+# Create Vercel-compatible output directory
 mkdir -p .vercel/output/static
 cp -r staticfiles/ .vercel/output/static/
 
+# Apply database migrations
 python3 manage.py makemigrations
 python3 manage.py migrate
