d1e31d72ad4552bca7b81a7e0e0596faee7f3069
anubus298 <araristaf@gmail.com>
2024-12-27 14:30:58 +0100

    fixes

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,7 @@ echo "Installing pip if required..."
 python3 -m ensurepip --upgrade
 python3 -m pip install --upgrade pip
 echo "Building the project..."
+python3 -m pip install setuptools
 python3 -m pip install -r requirements.txt
 echo "Make Migration..."
 echo "Collect Static..."
